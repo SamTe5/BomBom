14b882b8344ae6c771d1f649a30dbe9c248416dc
SametKiybik <samettemas05@gmail.com>
2024-08-18 12:29:16 +0300

    navigate

--- app/Pages/LoginPage/Login.jsx
+++ app/Pages/LoginPage/Login.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
-import {View,Image, TextInput, SafeAreaView,StyleSheet, Button} from 'react-native';
+import {Text,View,Image, TextInput, SafeAreaView,StyleSheet, Button,Alert} from 'react-native';
 import styles from "./loginStyle"
 
-function Login() {
+function Login({navigation}) {
+
+
   return (
     <SafeAreaView style={styles.container}>
       <View></View>
@@ -17,6 +19,9 @@ function Login() {
         <View style={styles.inputText}>
           <Image style={styles.kucukLogo} source={require("../../../src/images/phone.png")}/>
           <TextInput placeholder='Telefon Numarası'></TextInput>
+        </View>
+        <View>
+          <Text>Halen bir hesabın yoksa: <Text onPress={() => navigation.navigate("Register")}>Kayıt ol</Text> </Text>
         </View>
         <View>
           <Button color={"#ff969a"} title='Giriş Yap'  onPress={() => Alert.alert('Simple Button pressed')}/>
